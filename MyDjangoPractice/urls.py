ccd85719063b151e45bcbca3ee0296bf7a7863a0
Jeesang Kim <arsenalkim@gmail.com>
2017-02-21 22:45:10 +0900

    add to first page
    add to template, static ; base, home

--- MyDjangoPractice/urls.py
+++ MyDjangoPractice/urls.py
@@ -16,11 +16,15 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from MyDjangoPractice.views import HomeView
+
 #from django.views.generic import ListView, DetailView
 #from bookmark.models import Bookmark 
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    
+    url(r'^$', HomeView.as_view(), name='home'),
     url(r'^bookmark/', include('bookmark.urls', namespace='bookmark')),
     url(r'^blog/', include('blog.urls', namespace='blog')),
     
